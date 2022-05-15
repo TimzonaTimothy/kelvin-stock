ad0bd3d94aa03a2e9e8cae8116cd9fc2cc38cca8
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2022-05-15 18:41:24 +0100

    db

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib import messages
+
 
 def home(request):
     return render(request, "index.html", {})
@@ -9,4 +12,19 @@ def about(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        message_full_name = request.POST['full_name']  
+        message_email = request.POST['email']
+        message_subject = request.POST['message_subject']
+        message = request.POST['message']
+
+        send_mail(
+           message_full_name +' sent you an enquiry '+ message_email + ' \n'+ message_subject,
+            message,
+            'arizonatymothy@gmail.com',
+            ['arizonatymothy@gmail.com',],
+            fail_silently=False
+        )
+        messages.success(request, 'Dear ' + message_full_name+' Your email has been sent ','alert alert-success alert-dismissible')
+        return render(request, 'contact.html', {})
     return render(request, "contact.html", {})
